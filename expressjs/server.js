7b9132d2ac199d4ef17af353135111b3d476ac85
Piyush Jain <piyushmbm45@gmail.com>
2021-07-11 13:58:42 +0530

    setting up many routes

--- expressjs/server.js
+++ expressjs/server.js
@@ -3,11 +3,19 @@ const express = require('express')
 const app = express();
 
 
-app.get('/',(req,res)=>{
-    // res.send('Hello World its Express js');  // sending normal string
+// route - 1  (Home Route)
+app.get("/",(req,res)=>{
     res.send('<h1>Hello world</h1>');    // sending html syntax
 })
 
+// route - 2
+app.get("/contact",(req,res)=>{
+    res.send("Contact Me")
+})
 
+// route - 3
+app.get("/about", (req,res)=>{
+    res.send("About Me!")
+})
 
 app.listen(3000,()=> console.log("Hello World") )
