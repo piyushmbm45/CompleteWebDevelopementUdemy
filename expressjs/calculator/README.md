b5651fa0141d798ac3134169b1959aa98937fbb5
Piyush Jain <piyushmbm45@gmail.com>
2021-07-11 16:22:37 +0530

    sum of two numbers

---
+++ expressjs/calculator/README.md
@@ -0,0 +1,15 @@
+## Calculator
+
+#### multiplying two numbers
+
+- we can send file by using get request
+- use network section in inspect to see what data person entering or many more thing
+
+
+- to manipulate data from browser to backend we need data in backend from browser
+    - install body-parser
+
+- but if you have express 4.16 or later then you dont need body parser.  
+- res.send
+    - send the string not the number only
+        - see the calculator app.post -- if you send only result then it will give you error
--- expressjs/calculator/calculator.js
+++ expressjs/calculator/calculator.js
@@ -1,9 +1,19 @@
-const express = require("express")
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.get("/",(req,res)=>{
-res.send("Hello world")
-})
+app.post("/", (req, res) => {
+  let num1 = Number(req.body.n1);
+  let num2 = Number(req.body.n2);
+  let result = num1 + num2;
+  res.send("The Answer is : "+ result);
+});
 
-app.listen(3030,()=> console.log("listening on 3030"))
+app.listen(3030, () => console.log("listening on 3030"));
