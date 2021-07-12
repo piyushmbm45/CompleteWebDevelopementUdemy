1fc27c2d84f26abdb34b792e8dcb9f76a4307009
Piyush Jain <piyushmbm45@gmail.com>
2021-07-12 11:37:26 +0530

    learn res.write and get the response from ext. ser

---
+++ weatherproject/README.md
@@ -0,0 +1,30 @@
+## Weather Project
+
+### Connecting external server to Local Server
+
+#### In this project we are making an application which shows the data from other weather to our website using API.
+
+##### Tools Used
+
+- Nodejs
+    - expressjs
+    - nodemon
+    - native http module
+- postman
+
+##### Language Used
+
+- Javascript
+- Html
+- CSS
+
+
+
+
+- only one res.send allowed in one app.get
+    - bcz once we called send thats the end point there is no more send point
+
+- but by using res.write we send multiple data 😉.
+    - but dont use res.send with data then
+    - also end your server reponse by using res.end.
+            - i mean to say if you want to use res.send() with res.write then it should be blank to use the res.write without res.end
--- weatherproject/app.js
+++ weatherproject/app.js
@@ -5,6 +5,7 @@ const https = require("http");
 const app = express();
 
 app.get("/", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
   const url =
     "http://api.openweathermap.org/data/2.5/weather?q=London&appid=99a6930370afa07740f1c344aad5b588&units=metric";
   https.get(url, (response) => {
@@ -14,11 +15,14 @@ app.get("/", (req, res) => {
       const temp = weatherData.main.temp;
       const feelLike = weatherData.main.feels_like;
       const descri = weatherData.weather[0].description;
-      console.log(descri);
+      //   console.log(descri);
+      res.write(`<h1>The temperature in London is ${temp} °C. </h1>`);
+      res.write(`<p>The weather is currently ${descri}</p><br>`);
+      res.write(`<p>The weather is Feel Like ${feelLike}</p>`);
+      res.send();
+      // res.end();
     });
   });
-
-  res.send("Server is up and running");
 });
 
 app.listen(3000, () => console.log("listening on Port 3000"));
