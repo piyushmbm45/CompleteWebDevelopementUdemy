7acbf484d4b135a530bf7ddcbe78cb0db9c27a29
Piyush Jain <piyushmbm45@gmail.com>
2021-07-12 21:06:58 +0530

    add public folder - css - images

---
+++ newsletterSignup/README.md
@@ -0,0 +1,2 @@
+## News Letter Sign Up
+
--- newsletterSignup/app.js
+++ newsletterSignup/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/signup.html")
