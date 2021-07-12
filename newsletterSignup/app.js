21bfffefca248db5416f562230e45fb77e3730de
Piyush Jain <piyushmbm45@gmail.com>
2021-07-12 21:15:39 +0530

    post request homepage

--- newsletterSignup/app.js
+++ newsletterSignup/app.js
@@ -9,4 +9,11 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/signup.html")
 });
 
+app.post("/",(req,res)=>{
+    const fName = req.body.FirstName;
+    const lName = req.body.LastName;
+    const eMail = req.body.email;
+    console.log(`${fName} ${lName} ${eMail}`);
+})
+
 app.listen(3000, () => console.log("Listening On port 3000"));
