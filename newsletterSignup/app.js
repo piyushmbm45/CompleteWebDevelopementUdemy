a0f770418e4e2e49bbf4b06982ef85e12ae86239
Piyush Jain <piyushmbm45@gmail.com>
2021-07-12 21:00:34 +0530

    initialize files

---
+++ newsletterSignup/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signup.html")
+});
+
+app.listen(3000, () => console.log("Listening On port 3000"));
