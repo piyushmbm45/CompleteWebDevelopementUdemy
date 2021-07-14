c4271138caa6a5c96cfc82ac6e9046fb1236cc57
Piyush Jain <piyushmbm45@gmail.com>
2021-07-14 17:55:33 +0530

    request http

--- newsletterSignup/app.js
+++ newsletterSignup/app.js
@@ -1,33 +1,51 @@
 const express = require("express");
+const http = require("http");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/signup.html")
+  res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/",(req,res)=>{
-    const fName = req.body.FirstName;
-    const lName = req.body.LastName;
-    const eMail = req.body.email;
-    console.log(`${fName} ${lName} ${eMail}`);
+app.post("/", (req, res) => {
+  const fName = req.body.FirstName;
+  const lName = req.body.LastName;
+  const eMail = req.body.email;
+  console.log(`${fName} ${lName} ${eMail}`);
 
-    let data = {
-        members: [
-            {
-                email_address: email,
-                status: "subscribed",
-                merge_fields: {
-                    FNAME : fName,
-                    LNAME : lName
-                }
-            }
-        ]
-    }
-    const jsonData = JSON.stringify(data)
+  let data = {
+    members: [
+      {
+        email_address: eMail,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: fName,
+          LNAME: lName,
+        },
+      },
+    ],
+  };
+  const jsonData = JSON.stringify(data);
+
+  const url = "http://us6.api.mailchimp.com/3.0/lists/b6217979bf";
+  const options = {
+      method : "POST",
+      auth: "piyushmbm:api_key"
+  }
+
+  const request = http.request(url, options, (response) => {
+      response.on("data",(data)=>{
+          console.log(JSON.parse(data));
+      })
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
 app.listen(3000, () => console.log("Listening On port 3000"));
+
+
