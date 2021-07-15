6fefb307b9f8ff279f495da92c27d88b84922eab
Piyush Jain <piyushmbm45@gmail.com>
2021-07-15 18:10:09 +0530

    challange- print day on screen

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const ejs = require("ejs");
-const { addListener } = require("nodemon");
 
 const app = express();
 
@@ -9,11 +8,28 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   const today = new Date();
     let day = "";
-  if (today.getDay() === 6 || today.getDay() === 0) {
-    day = "Weekand";
-  } else {
-    day = "Weekday";
+  if (today.getDay() === 0) {
+    day = "Sunday";
+  } else if(today.getDay() === 1) {
+    day = "Monday";
+
+  } else if (today.getDay() === 2){
+    day = "Tuesday";
+
+  } else if (today.getDay() === 3){
+    day = "Wednesday";
+
+  }else if (today.getDay() === 4){
+    day = "Thursday";
+
+  } else if (today.getDay() === 5){
+    day = "Friday";
+
+  } else{
+    day = "Saturday";
   }
+
+  
 res.render("list",{kindOfDay : day});
 });
 
