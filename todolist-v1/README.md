e8f26d3de1f35a01480e9b38ea3f69e81092bd15
Piyush Jain <piyushmbm45@gmail.com>
2021-07-15 21:29:17 +0530

    Solve Error -> add new list item

--- todolist-v1/README.md
+++ todolist-v1/README.md
@@ -8,3 +8,10 @@
 
 
 - EJS - embedded javascript 
+
+
+- the problem is that when render post request browser gives that newListItem is not defined
+
+- bcz we are in home route('/') and render list we defined only one variable kindOfDay whereas our list.ejs file also have one more variable newListItem
+
+- so we need to define newListItem in Home route
--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
-
+var data = "";
 app.get("/", (req, res) => {
   const today = new Date();
   const options = {
@@ -18,19 +18,14 @@ app.get("/", (req, res) => {
 
   const day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day });
+  res.render("list", { kindOfDay: day, newListItem: data });
 });
-
-
 
 app.post("/", (req, res) => {
-  const data = req.body.newItem;
-  res.render("list",{newData : data});
+  data = req.body.newItem;
+  res.redirect("/");
   console.log(data);
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on Port ${port}`));
