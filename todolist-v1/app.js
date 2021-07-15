106b017d06ad467e263d24bb14483e99c78252e1
Piyush Jain <piyushmbm45@gmail.com>
2021-07-15 20:49:35 +0530

    Date Print In Local Format

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -7,33 +7,14 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   const today = new Date();
-  const currDay = today.getDay();
-  let day = "";
-  switch (currDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-    console.log(`Error : The current Day is ${currDay}`);
-  }
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    // year: "numeric",
+  };
+
+  const day = today.toLocaleDateString("en-US",options);
 
   res.render("list", { kindOfDay: day });
 });
