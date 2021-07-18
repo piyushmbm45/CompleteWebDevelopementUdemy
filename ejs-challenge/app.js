b91f963a21ff6189e62fde403b5592dbc7d00026
Piyush Jain <piyushmbm45@gmail.com>
2021-07-18 16:56:49 +0530

    Add contact and about route

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const ejs = require("ejs");
+const { get } = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -16,11 +17,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+
+// homepage route 
 app.get("/", (req, res) => {
-
-
-  res.render("home.ejs", {homeContent : homeStartingContent});
+  res.render("home.ejs", { homeContent: homeStartingContent });
 });
+
+// about page route
+app.get("/about",(req,res)=>{
+  res.render("about.ejs", {about : aboutContent});
+})
+
+// contact page route
+app.get("/contact",(req,res)=>{
+  res.render("contact.ejs",{contact: contactContent});
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
