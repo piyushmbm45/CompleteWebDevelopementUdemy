3548ce81ca008896c70baf6994f1a8f3bdc41899
Piyush Jain <piyushmbm45@gmail.com>
2021-07-22 18:29:11 +0530

    validation of input data

--- fruits-projects/app.js
+++ fruits-projects/app.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/fruitsDB", {
-  useUnifiedTopology: true,
-});
+mongoose.connect("mongodb://localhost:27017/fruitsDB",{ useNewUrlParser: true,
+useUnifiedTopology: true});
 
 const fruitSchema = new mongoose.Schema({
   name: String,
-  rating: Number,
+  rating: {
+      type: Number,
+      min: 1,
+      max: 10
+  },
   review: String,
 });
 
@@ -14,62 +17,62 @@ const Fruit = mongoose.model("Fruit", fruitSchema);
 
 const fruit = new Fruit({
   name: "Apple",
-  rating: 8,
+  rating: 11,
   review: "Pretty Solid",
 });
 
-// fruit.save();
+fruit.save();
 
-const personSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-});
+// const personSchema = new mongoose.Schema({
+//   name: String,
+//   age: Number,
+// });
 
-const Person = mongoose.model("person", personSchema);
+// const Person = mongoose.model("person", personSchema);
 
-const person = new Person({
-  name: "John",
-  age: 56,
-});
+// const person = new Person({
+//   name: "John",
+//   age: 56,
+// });
 
-// person.save();
+// // person.save();
 
-const mango = new Fruit({
-  name: "mango",
-  rating: 10,
-  review: "King",
-});
-const orange = new Fruit({
-  name: "orange",
-  rating: 6,
-  review: "Soar Fruit Vitamin C",
-});
-const banana = new Fruit({
-  name: "banana",
-  rating: 8,
-  review: "Power Energy",
-});
+// const mango = new Fruit({
+//   name: "mango",
+//   rating: 10,
+//   review: "King",
+// });
+// const orange = new Fruit({
+//   name: "orange",
+//   rating: 6,
+//   review: "Soar Fruit Vitamin C",
+// });
+// const banana = new Fruit({
+//   name: "banana",
+//   rating: 8,
+//   review: "Power Energy",
+// });
 
-// insert many data in single run
-// Fruit.insertMany([mango,orange,banana],(err)=>{
-//     if(err){
-//         console.log(err);
-//     }
-//     else{
-//         console.log("Data successfully uploaded");
-//     }
-// })
+// // insert many data in single run
+// // Fruit.insertMany([mango,orange,banana],(err)=>{
+// //     if(err){
+// //         console.log(err);
+// //     }
+// //     else{
+// //         console.log("Data successfully uploaded");
+// //     }
+// // })
 
-// to find your data in database
+// // to find your data in database
 
-// returns an array of all data which means we can apply all the method of java script
-Fruit.find((err, fruits) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(fruits);
-  }
-});
+// // returns an array of all data which means we can apply all the method of java script
+// Fruit.find((err, fruits) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(fruits);
+//   }
+// });
 
 Fruit.find((err, fruits) => {
     if(err){
