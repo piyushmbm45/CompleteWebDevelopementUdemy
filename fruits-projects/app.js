cbcbe40dbfc356b7ff131574b04da8f8a622c7da
Piyush Jain <piyushmbm45@gmail.com>
2021-07-22 18:09:29 +0530

    accessing data from app.js

--- fruits-projects/app.js
+++ fruits-projects/app.js
@@ -1,61 +1,82 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true });
-
-
-const fruitSchema = new mongoose.Schema ({
-    name: String,
-    rating : Number,
-    review: String
+mongoose.connect("mongodb://localhost:27017/fruitsDB", {
+  useUnifiedTopology: true,
 });
 
-const Fruit = mongoose.model("Fruit",fruitSchema);
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String,
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
 
 const fruit = new Fruit({
-    name : "Apple",
-    rating : 8,
-    review: "Pretty Solid"
+  name: "Apple",
+  rating: 8,
+  review: "Pretty Solid",
 });
 
 // fruit.save();
 
-
 const personSchema = new mongoose.Schema({
-    name : String,
-    age : Number
-})
+  name: String,
+  age: Number,
+});
 
-const Person = mongoose.model("person",personSchema);
+const Person = mongoose.model("person", personSchema);
 
 const person = new Person({
-    name: "John",
-    age : 56
+  name: "John",
+  age: 56,
 });
 
 // person.save();
 
-
 const mango = new Fruit({
-    name: "mango",
-    rating: 10,
-    review: "King"
-})
+  name: "mango",
+  rating: 10,
+  review: "King",
+});
 const orange = new Fruit({
-    name: "orange",
-    rating: 6,
-    review: "Soar Fruit Vitamin C"
-})
+  name: "orange",
+  rating: 6,
+  review: "Soar Fruit Vitamin C",
+});
 const banana = new Fruit({
-    name: "banana",
-    rating: 8,
-    review: "Power Energy"
-})
+  name: "banana",
+  rating: 8,
+  review: "Power Energy",
+});
 
-Fruit.insertMany([mango,orange,banana],(err)=>{
+// insert many data in single run
+// Fruit.insertMany([mango,orange,banana],(err)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log("Data successfully uploaded");
+//     }
+// })
+
+// to find your data in database
+
+// returns an array of all data which means we can apply all the method of java script
+Fruit.find((err, fruits) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(fruits);
+  }
+});
+
+Fruit.find((err, fruits) => {
     if(err){
         console.log(err);
+    }else{
+        fruits.forEach((ele)=>{
+            console.log(ele.name);
+        })
     }
-    else{
-        console.log("Data successfully uploaded");
-    }
-})
+});
