778a79e420847824a35f56f89e338b9a0995c0fc
Piyush Jain <piyushmbm45@gmail.com>
2021-07-23 09:20:33 +0530

    minimize the using for loop

--- dom-dum-kit-chall./Drum Kit Starting Files/index.js
+++ dom-dum-kit-chall./Drum Kit Starting Files/index.js
@@ -1,60 +1,70 @@
-// 1.
-const btn1 = document.querySelector(".w");
+// // 1.
+// const btn1 = document.querySelector(".w");
 
-btn1.addEventListener("click",()=>{
-    const audio = new Audio("sounds/crash.mp3")
-    audio.play();
-})
+// btn1.addEventListener("click",()=>{
+//     const audio = new Audio("sounds/crash.mp3")
+//     audio.play();
+// })
 
-// 2.
-const btn2 = document.querySelector(".a");
+// // 2.
+// const btn2 = document.querySelector(".a");
 
-btn2.addEventListener("click",()=>{
-    const audio = new Audio("sounds/kick-bass.mp3")
-    audio.play();
-})
+// btn2.addEventListener("click",()=>{
+//     const audio = new Audio("sounds/kick-bass.mp3")
+//     audio.play();
+// })
 
-// 3. 
-const btn3 = document.querySelector(".s");
+// // 3. 
+// const btn3 = document.querySelector(".s");
 
-btn3.addEventListener("click",()=>{
-    const audio = new Audio("sounds/snare.mp3")
-    audio.play();
-})
+// btn3.addEventListener("click",()=>{
+//     const audio = new Audio("sounds/snare.mp3")
+//     audio.play();
+// })
 
-// 4.
+// // 4.
 
-const btn4 = document.querySelector(".d");
+// const btn4 = document.querySelector(".d");
 
-btn4.addEventListener("click",()=>{
-    const audio = new Audio("sounds/tom-1.mp3")
-    audio.play();
-})
+// btn4.addEventListener("click",()=>{
+//     const audio = new Audio("sounds/tom-1.mp3")
+//     audio.play();
+// })
 
-// 5. 
+// // 5. 
 
-const btn5 = document.querySelector(".j");
+// const btn5 = document.querySelector(".j");
 
-btn5.addEventListener("click",()=>{
-    const audio = new Audio("sounds/tom-2.mp3")
-    audio.play();
-})
+// btn5.addEventListener("click",()=>{
+//     const audio = new Audio("sounds/tom-2.mp3")
+//     audio.play();
+// })
 
 
-// 6. 
+// // 6. 
 
-const btn6 = document.querySelector(".k");
+// const btn6 = document.querySelector(".k");
 
-btn6.addEventListener("click",()=>{
-    const audio = new Audio("sounds/tom-3.mp3")
-    audio.play();
-})
+// btn6.addEventListener("click",()=>{
+//     const audio = new Audio("sounds/tom-3.mp3")
+//     audio.play();
+// })
 
-// 7.
+// // 7.
 
-const btn7 = document.querySelector(".l");
+// const btn7 = document.querySelector(".l");
 
-btn7.addEventListener("click",()=>{
-    const audio = new Audio("sounds/tom-4.mp3")
-    audio.play();
-})
+// btn7.addEventListener("click",()=>{
+//     const audio = new Audio("sounds/tom-4.mp3")
+//     audio.play();
+// })
+
+
+
+const btnNum = document.querySelectorAll(".drum").length;
+
+for(let i=0;i<=btnNum;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",()=>{
+            const audio = new Audio(`sounds/${i}.mp3`)
+            audio.play();
+    })};
