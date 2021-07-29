bcfe88f413cb942ccaf811e9840177f97cc0ce60
Piyush Jain <piyushmbm45@gmail.com>
2021-07-29 17:25:33 +0530

    add mongoose

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -1,19 +1,44 @@
 const express = require("express");
 const ejs = require("ejs");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 app.set("view engine", "ejs");
 
+const itemsSchema = {
+  name: String,
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome To Your todolist.",
+});
+const item2 = new Item({
+  name: "Hit the + Button to add a new Item.",
+});
+const item3 = new Item({
+  name: "<-- Hit This to delete an item.",
+});
+
+const defaultItems = [item1,item2,item3];
 
 
-
-
-
+Item.insertMany(defaultItems,(err)=>{
+  if(err){
+    console.log(err);
+  }else{
+    console.log("Items successfully Inseted");
+  }
+});
 
 
 app.get("/", (req, res) => {
@@ -27,7 +52,7 @@ app.get("/", (req, res) => {
 
   const day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { listTitle: day, newListItems: items });
+  res.render("list", { listTitle: "Today", newListItems: items });
 });
 
 app.post("/", (req, res) => {
@@ -46,9 +71,9 @@ app.get("/work", (req, res) => {
   res.render("list", { listTitle: "Work List", newListItems: newItem });
 });
 
-app.get("/about",(req,res)=>{
-  res.render("about")
-})
+app.get("/about", (req, res) => {
+  res.render("about");
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on Port ${port}`));
