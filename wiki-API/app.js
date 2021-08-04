2248163053fb441df1a6d6b0c44a2822c761420d
Piyush Jain <piyushmbm45@gmail.com>
2021-08-04 10:34:03 +0530

    chained route handler

--- wiki-API/app.js
+++ wiki-API/app.js
@@ -1,74 +1,60 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const ejs = require('ejs')
-
+const express = require("express");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"))
-
+app.use(express.static("public"));
 
 // mongoDb connection
 mongoose.connect("mongodb://127.0.0.1:27017/wikiDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
+});
 
 // schema for database
 const articleSchema = {
-    title: String,
-    content: String
-}
-const Article = new mongoose.model('Article',articleSchema)
+  title: String,
+  content: String,
+};
+const Article = new mongoose.model("Article", articleSchema);
 
-// getting all the articles
-app.get('/articles',(req,res)=>{
-    Article.find({},(err,foundArticles)=>{
-        if(!err){
-            res.send(foundArticles)
-        }
-        else{
-            res.send(err)
-        }
-    })
-})
-
-// post a new article
-app.post('/articles',(req,res)=>{
-    
-    const newArticle = new Article({
-        title : req.body.title,
-        content : req.body.content
-    })
-    newArticle.save((err)=>{
-        if(!err){
-            res.send('Successfully send the new article')
-        }
-        else{
-            res.send(err)
-        }
+app.route("/articles")
+  .get((req, res) => {
+    Article.find({}, (err, foundArticles) => {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
     });
-})
-
-// delete article or articles
-
-app.delete('/articles',(req,res)=>{
-    Article.deleteMany({},(err)=>{
-        if(!err){
-            res.send("all articles are successfull deleted");
-        }
-        else{
-            res.send(err);
-        }
+  })
+  .post((req, res) => {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    newArticle.save((err) => {
+      if (!err) {
+        res.send("Successfully send the new article");
+      } else {
+        res.send(err);
+      }
     })
-})
+  })
+  .delete((req, res) => {
+    Article.deleteMany({}, (err) => {
+      if (!err) {
+        res.send("all articles are successfull deleted");
+      } else {
+        res.send(err);
+      }
+    })
+  });
 
-
-
-
-app.listen(3000,()=>{
-    console.log("listening on 3000");
-})
+app.listen(3000, () => {
+  console.log("listening on 3000");
+});
