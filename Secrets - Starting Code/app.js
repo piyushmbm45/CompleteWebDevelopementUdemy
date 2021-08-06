34dcaa824705250484ed8a9f3a688ecde35096d9
Piyush Jain <piyushmbm45@gmail.com>
2021-08-06 16:49:43 +0530

    adding user schema to mongoDb

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const ejs = require('ejs')
+const mongoose = require('mongoose')
 
 
 const app = express();
@@ -8,6 +9,21 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+
+////////mongodb connection
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const userSchema = {
+    email: String,
+    password: String
+}
+
+const User =  new mongoose.model("User",userSchema);
+
 
 app.get('/',(req,res)=>{
     res.render("home")
