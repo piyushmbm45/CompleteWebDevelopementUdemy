a99685ad5d1c58ecfee63b840b0bc804508c79a7
Piyush Jain <piyushmbm45@gmail.com>
2021-08-06 16:32:16 +0530

    Learning Auth and Security - Initializing Files

---
+++ Secrets - Starting Code/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const ejs = require('ejs')
+
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("listening on 3000");
+  });
