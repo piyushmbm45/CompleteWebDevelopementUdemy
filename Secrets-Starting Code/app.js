b1bd49a896b72c0e8fd9784810c758c23d2810bf
Piyush Jain <piyushmbm45@gmail.com>
2021-08-08 22:32:57 +0530

    putting all secrets on one page

--- Secrets-Starting Code/app.js
+++ Secrets-Starting Code/app.js
@@ -110,11 +110,16 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/secrets", (req, res) => {
-  if (req.isAuthenticated()) {
-    res.render("secrets");
-  } else {
-    res.redirect("/login");
-  }
+  
+  User.find({"secret": {$ne:null}}, (err,foundUsers)=>{
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUsers) {
+          res.render("secrets",{usersWithSecrets : foundUsers});
+      }
+    }
+  })
 });
 
 // submit secret route
