961be7ad6d360466b47b9654309c8a316616d558
Piyush Jain <piyushmbm45@gmail.com>
2021-08-08 13:45:42 +0530

    login - passport

--- Secrets-Starting Code/app.js
+++ Secrets-Starting Code/app.js
@@ -87,6 +87,21 @@ app.post("/register", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
+
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  })
+  req.login(user,(err)=>{
+    if(err){
+      console.log(err);
+    }
+    else{
+      passport.authenticate('local')(req,res,()=>{
+        res.redirect('/secrets')
+      })
+    }
+  })
 });
 
 app.listen(3000, () => {
