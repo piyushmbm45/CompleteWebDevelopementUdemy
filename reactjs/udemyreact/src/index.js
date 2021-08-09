398ce8a314c8092f2efe69210a45920d7c179ca7
Piyush Jain <piyushmbm45@gmail.com>
2021-08-09 12:29:12 +0530

    challenge 2

--- reactjs/udemyreact/src/index.js
+++ reactjs/udemyreact/src/index.js
@@ -2,10 +2,25 @@ import React from "react";
 import { render } from "react-dom";
 
 // we are using html in js file this is done by react it compile html into js
+// task 1
+// render(
+//   <div>
+//     <h1>Hello World!</h1>
+//     <p>This is Paragraph</p>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+// task 2
+
 render(
   <div>
-    <h1>Hello World!</h1>
-    <p>This is Paragraph</p>
+  <h1>My fav Chrome Ext</h1>
+    <ul>
+      <li>Ad Blocker</li>
+      <li>JSON pro</li>
+      <li>PIP</li>
+    </ul>
   </div>,
   document.getElementById("root")
 );
