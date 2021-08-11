8862705cc880a0c53f14f15d1f650ecc2d2f9238
Piyush Jain <piyushmbm45@gmail.com>
2021-08-11 23:04:22 +0530

    styling the final output

--- reactjs/contact-project/src/components/card.jsx
+++ reactjs/contact-project/src/components/card.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-function Card(props){
-    return(
-        <div>
+function Card(props) {
+  return (
+    <div className="container">
+      <div className="top">
         <h2>{props.name}</h2>
-        <img src={props.img} alt="img"/>
+        <img src={props.img} alt="img" />
+      </div>
+      <div className="bottom">
         <p>{props.tel}</p>
         <p>{props.email}</p>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Card;
