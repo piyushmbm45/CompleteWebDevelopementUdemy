ea8ca8a68738549fe74b782421450cb8321901c1
Piyush Jain <piyushmbm45@gmail.com>
2021-08-11 18:12:43 +0530

    outline made, added all the contacts to index

--- reactjs/contact-project/src/components/App.jsx
+++ reactjs/contact-project/src/components/App.jsx
@@ -1,12 +1,30 @@
-import React from "react"
-import Card from "./card"
+import React from "react";
+import Card from "./card";
+import ContactArray from "../contact.js";
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <h1>My Contacts</h1>
-    <Card name="Jain" img="" tel="98765433" email="abc@gmail.com"/>
+      <h1>My Contacts</h1>
+      <Card
+        name={ContactArray[0].name}
+        img={ContactArray[0].imgURL}
+        tel={ContactArray[0].phone}
+        email={ContactArray[0].email}
+      />
+      <Card
+        name={ContactArray[1].name}
+        img={ContactArray[1].imgURL}
+        tel={ContactArray[1].phone}
+        email={ContactArray[1].email}
+      />
+      <Card
+        name={ContactArray[2].name}
+        img={ContactArray[2].imgURL}
+        tel={ContactArray[2].phone}
+        email={ContactArray[2].email}
+      />
     </div>
   );
 }
