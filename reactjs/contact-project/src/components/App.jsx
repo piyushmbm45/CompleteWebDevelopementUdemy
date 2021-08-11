7aaae60dffa95397ace760b00f58cfd916226698
Piyush Jain <piyushmbm45@gmail.com>
2021-08-11 17:45:30 +0530

    Initializing contact project -use of com and props

---
+++ reactjs/contact-project/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Card from "./card"
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>My Contacts</h1>
+    <Card name="Jain" img="" tel="98765433" email="abc@gmail.com"/>
+    </div>
+  );
+}
+
+export default App;
