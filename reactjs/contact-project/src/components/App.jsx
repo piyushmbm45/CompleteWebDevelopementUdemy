5cc5fcc91a3682eea79820783f2f91a957da9dbc
Piyush Jain <piyushmbm45@gmail.com>
2021-08-12 17:56:37 +0530

    map method to create optimized code

--- reactjs/contact-project/src/components/App.jsx
+++ reactjs/contact-project/src/components/App.jsx
@@ -3,29 +3,23 @@ import Card from "./card";
 import ContactArray from "../contact.js";
 import Avatar from "./avatar";
 
+function createCard(ele) {
+  return (
+    <Card
+      name={ele.name}
+      img={ele.imgURL}
+      tel={ele.phone}
+      email={ele.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <h1>My Contacts</h1>
       <Avatar img="https://upload.wikimedia.org/wikipedia/commons/7/70/Lilli_Iliev%2C_Portrait.jpg" />
-      <Card
-        name={ContactArray[0].name}
-        img={ContactArray[0].imgURL}
-        tel={ContactArray[0].phone}
-        email={ContactArray[0].email}
-      />
-      <Card
-        name={ContactArray[1].name}
-        img={ContactArray[1].imgURL}
-        tel={ContactArray[1].phone}
-        email={ContactArray[1].email}
-      />
-      <Card
-        name={ContactArray[2].name}
-        img={ContactArray[2].imgURL}
-        tel={ContactArray[2].phone}
-        email={ContactArray[2].email}
-      />
+      {ContactArray.map(createCard)}
     </div>
   );
 }
