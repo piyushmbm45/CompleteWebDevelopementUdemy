c81bb22cb783ec309422c2d5fe844c0c1bea0799
Piyush Jain <piyushmbm45@gmail.com>
2021-08-20 07:38:51 +0530

    delete function added

--- reactjs/my-app/src/components/app.jsx
+++ reactjs/my-app/src/components/app.jsx
@@ -5,20 +5,37 @@ import Note from "./note";
 import Createarea from "./createarea";
 
 function App() {
+  const [inputArray, setinputArray] = useState([]);
+  function addNote(note) {
+    setinputArray((preValue) => {
+      return [...preValue, note];
+    });
+  }
 
-  const [inputArray,setinputArray] = useState([])
-function addNote(note){
-setinputArray(preValue=>{
-  return [...preValue, note]
-})
-}
+  function deleteNote(id){
+   setinputArray(preValue=>{
+     return preValue.filter((note,index)=>{
+       return index !== id;
+     })
+   })
+  }
+
+
   return (
     <div>
       <Header />
-      <Createarea onAdd={addNote}/>
-      {inputArray.map((ele,index)=>{
-       return( <Note key={index} id={index} title={ele.title} content={ele.content}/>
-      )})}
+      <Createarea onAdd={addNote} />
+      {inputArray.map((ele, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={ele.title}
+            content={ele.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
--- reactjs/my-app/src/components/note.jsx
+++ reactjs/my-app/src/components/note.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 function Note(props) {
+  function handleDelete(){
+    return props.onDelete(props.id)
+  }
   return (
     <div className="note">
     <h2>{props.title}</h2>
     <p>{props.content}</p>
-    <button>Delete</button>
+    <button onClick={handleDelete}>Delete</button>
     </div>
   );
 }
